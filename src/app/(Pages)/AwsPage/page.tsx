580b56149c56573a349e205c67f57c34a96bd462
Guilherme Tavares e Tavares Silva <guittsilva@MacBook-Pro-de-Guilherme.local>
2024-04-11 22:07:21 -0300

    FEAT: Criar banner subTitle + Banner de Informacoes da página da AWS

--- src/app/(Pages)/AwsPage/page.tsx
+++ src/app/(Pages)/AwsPage/page.tsx
@@ -18,16 +18,40 @@ const AwsPage = () => {
                 </div>
             </s.TitleBox>
 
-            <s.SimulatesBox>
-                {allSimulates.map((item, index) => (
-                    <div className="singleSimulate">
-                        <Link href={"/"} key={index}>
-                            Simulado 0{index + 1}
-                        </Link>
-                        <p> {item.length} Questões.</p> 
+            <s.SubTitleBox>
+                <div className="leftSide">
+                    <span>AWS CLOUD PRACTIONER ESSENTIALS</span>
+                    <h3> Material AWS Gratuito em Português</h3>
+                    <p> Aqui voce encontra material escrito, resumos quizzes e dicas.</p>
+                </div>
+                <div className="rightSide">
+                    <a href="">Veja Mais</a>
+                </div>
+            </s.SubTitleBox>
+
+            <s.InfosBox>
+                <h2> Visão geral do exame:</h2>
+
+                <div className="bigBox">
+                    <div className="leftSide">
+                        <p><span>Categoria: </span> Básico</p>
+                        <p><span>Duração do exame: </span> 90 Minutos </p>
+                        <p><span>Formato do Exame: </span> 65 questões; múltipla escolha ou múltipla resposta</p>
+                        <p><span>Custo: </span> 100 USD. Visite os Preços do exame para obter informações adicionais sobre custos, incluindo taxas de câmbio</p>
+                        <p><span>Teste presencial ou on-line: </span> Centro de testes da Pearson VUE ou exame on-line monitorado</p>
+                        <p><span>Idiomas oferecidos: </span> Inglês, japonês, coreano, chinês simplificado, chinês tradicional, bahasa (Indonésia), espanhol (Espanha), espanhol (América Latina), francês (França), alemão, italiano e português (Brasil)</p>
                     </div>
-                ))}
-            </s.SimulatesBox>
+                    <div className="rightSide">
+                        <p>O <span>AWS Certified Cloud Practitioner</span> valida a compreensão básica e de alto nível dos serviços, da terminologia e da Nuvem AWS.  Esse é um bom ponto de partida na jornada de certificação da AWS para indivíduos sem experiência anterior em TI ou nuvem que estão migrando para uma carreira na nuvem ou para funcionários da linha de negócios que buscam conhecimento básico sobre a nuvem.</p>
+                        <div className="img">
+
+                        </div>
+                    </div>
+                </div>
+            </s.InfosBox>
+
+
+
 
 
         </s.Container>
--- src/app/(Pages)/AwsPage/style.ts
+++ src/app/(Pages)/AwsPage/style.ts
@@ -57,6 +57,97 @@ export const TitleBox = styled.div`
         }
     }
 `
+export const SubTitleBox = styled.div`
+    height: 160px;
+    width: 100%;
+    background-color: #232F3F;
+    display: flex;
+    div{
+        flex: 1;
+    }
+    .leftSide{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-left: 20px;
+        color: white;
+        gap: 10px;
+        span{
+            color: orange;
+            font-weight: bolder;
+            font-style: italic;
+        }
+        p{
+            font-size: 18px;
+        }
+    }
+    .rightSide{
+        display: flex;
+        align-items: center;
+        justify-content: end;
+        padding-right: 20px;
+        
+        a{
+            color: white;
+            font-size: 20px;
+            border: 2px solid white;
+            padding: 10px 40px;
+            &:hover{
+                color: orange;
+                border-color: orange;
+            }
+        }
+    }
+`
+
+export const InfosBox = styled.div` 
+    min-height: 500px;
+    width: 100%;
+
+    h2{
+        margin: 20px;
+        font-size: 25px;
+    }
+
+    .bigBox{
+        display: flex;
+        padding: 20px;
+        div{
+            flex: 1;
+        }
+        .leftSide{
+            font-size: 17px;
+            display: flex;
+            flex-direction: column;
+            gap: 20px;
+            
+            span{
+                font-weight: bolder;
+                font-size: 20px;
+            }
+        }
+
+        .rightSide{
+            p{
+                font-size: 23px;
+               
+            }
+            span{
+                font-weight: bolder;
+                font-style: italic;
+            }
+            .img{
+                background-image: url(https://d1.awsstatic.com/training-and-certification/certification-badges/AWS-Certified-Cloud-Practitioner_badge.634f8a21af2e0e956ed8905a72366146ba22b74c.png);
+                height: 300px;
+                width: 300px;
+                margin: 10px auto;
+
+            }
+        }
+    }
+`
+
+
 export const SimulatesBox = styled.div` 
     border: 3px solid gray;
     min-height: 300px;
