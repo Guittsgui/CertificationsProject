5b55e92ac4e1f532e46d1bd87c38087a271df37b
Guilherme Tavares e Tavares Silva <guittsilva@MacBook-Pro-de-Guilherme.local>
2024-04-11 21:35:22 -0300

    FEAT: Criar Banner da Página Home da AWS

--- src/app/(Pages)/AwsPage/page.tsx
+++ src/app/(Pages)/AwsPage/page.tsx
@@ -7,7 +7,16 @@ const AwsPage = () => {
 
     return(
         <s.Container>
-            <h2> AWS Page</h2>
+            <s.TitleBox>
+                <h2> AWS Certified Cloud Practioner (CLF-C02)</h2>
+                <p> Saiba mais sobre esta Certificação e os recursos da AWS que podem ajuda-lo a se preparar.</p>
+                <div className="bt">
+                    <button className='orange'> 
+                        <a href="https://www.aws.training/certification/?cta=cptopbanner" target='_blank'>Agende um exame</a>
+                    </button>
+                    <button> <a href="" target='_blank'>Prepare-se para o exame</a></button>
+                </div>
+            </s.TitleBox>
 
             <s.SimulatesBox>
                 {allSimulates.map((item, index) => (
--- src/app/(Pages)/AwsPage/style.ts
+++ src/app/(Pages)/AwsPage/style.ts
@@ -5,8 +5,56 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
+`
+
+export const TitleBox = styled.div`
+    height: 250px;
+    width: 100%;
+    background: rgb(237,251,63);
+    background: radial-gradient(circle, rgba(237,251,63,1) 0%, rgba(252,161,70,1) 100%);    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    padding-left: 70px;
+    gap: 10px;
     h2{
-        margin: 20px;
+        font-size: 32px;
+        color: white;
+        text-shadow: 2px 2px 2px black;
+    }
+    p{
+        font-size: 20px;
+    }
+    button{
+        width: 280px;
+        height: 40px;
+        border-radius: 30px;
+        border: none;
+        margin-top: 20px;
+        background-color: white;
+        border: 2px solid blue;
+        &:hover{
+            border-color: black;
+        }
+        a{
+            color: blue;
+        }
+    }
+    .bt{
+        display: flex;
+        align-items: center;
+        gap: 30px;
+    }
+    .orange{
+        background-color: orange;
+        font-weight: bolder;
+        border: 1px solid gray;
+        a{
+            color: black;
+        }
+        &:hover{
+            background-color: darkorange;
+        }
     }
 `
 export const SimulatesBox = styled.div` 
