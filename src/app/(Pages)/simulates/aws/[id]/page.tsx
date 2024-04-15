35d4e2d8796a1a8c9e94ebc93dc723a1d3209fda
Guilherme Tavares e Tavares Silva <guittsilva@MacBook-Pro-de-Guilherme.local>
2024-04-15 18:46:56 -0300

    FEAT: Implementar questoes + Paginacao das qeustoes de cada simulado

--- src/app/(Pages)/simulates/aws/[id]/page.tsx
+++ src/app/(Pages)/simulates/aws/[id]/page.tsx
@@ -8,15 +8,44 @@ const MainPage = ({params}: any) => {
     
     const simulate = allSimulates[params.id]
     const [hasStarted, setHasStarted] = useState(false);
+    const [currentQuestion , setCurrentQuestion] = useState(0)
 
     return(
 
         <s.body>
-            <s.initialContainer>
+            <s.initialContainer hasStarted={hasStarted}>
                 <h2> Simulado {parseInt(params.id) + 1} </h2>
                 <p> {simulate.length} Questões.</p>
                 <button onClick={() => setHasStarted(!hasStarted)}> Iniciar </button>
             </s.initialContainer>
+            <s.questionsContainer hasStarted={hasStarted}>
+                <s.questionTitle>
+                    <p> Questão {currentQuestion + 1}/ {simulate.length}</p>
+                </s.questionTitle>
+                <s.questionBody>
+                    <s.question>
+                         <p> <span>Pergunta: </span>{simulate[currentQuestion].question} </p>
+                    </s.question>
+                    <s.answers>
+                        {simulate[currentQuestion].answers.map((item, index) => (
+                            <p key={index}> {item} </p>
+                        ))}
+                    </s.answers>
+                </s.questionBody>
+                <s.questionFooter>
+                    <button 
+                        disabled={currentQuestion === 0}
+                        onClick={() => setCurrentQuestion(currentQuestion - 1)}> 
+                            Anterior 
+                    </button>
+                    <button 
+                        disabled ={currentQuestion === simulate.length - 1}
+                        onClick={() => setCurrentQuestion(currentQuestion + 1)}> 
+                            Próxima 
+                    </button>
+                </s.questionFooter>
+            </s.questionsContainer>
+
         </s.body>
     )
 }
--- src/app/(Pages)/simulates/aws/[id]/style.ts
+++ src/app/(Pages)/simulates/aws/[id]/style.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
+type props = {
+    hasStarted: boolean;
+}
+
 export const body = styled.div`
     min-height: 750px;
     display: flex;
@@ -8,10 +12,10 @@ export const body = styled.div`
     flex-direction: column;
 `
 
-export const initialContainer = styled.div` 
+export const initialContainer = styled.div<props>` 
     width: 650px;
     height: 600px;
-    display: flex;
+    display: ${(props) => props.hasStarted === true ? "none" : "flex"};
     align-items: center;
     justify-content: center;
     flex-direction: column;
@@ -43,6 +47,82 @@ export const initialContainer = styled.div`
     }
 `
 
-export const questionsContainer = styled.div`
+export const questionsContainer = styled.div<props>`
+    width: 650px;
+    min-height: 600px;
+    display: ${(props) => props.hasStarted === true ? "flex" : "none"};
+    align-items: center;
 
+    flex-direction: column;
+    border: 1px solid black;
+    border-radius: 20px;
+    box-shadow: 2px 2px 2px gray;
 `
+
+export const questionTitle = styled.header`
+    height: 100px;
+    width: 100%;
+    border-bottom: 2px solid black;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 25px;
+    p{
+        border-bottom: 3px solid orange;
+    }
+`
+
+export const questionFooter = styled.div`
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: center;
+    height: 100px;
+    gap: 50px;
+
+    button{
+        background-color: orange;
+        border: none;
+        border-radius: 20px;
+        width: 200px;
+        height: 40px;
+        border: 1px solid black;
+        font-size: 18px;
+        &:hover{
+            opacity: 70%;
+        }
+        &:disabled{
+            background-color: darkgray;
+            cursor: not-allowed;
+            opacity: 100%;
+        }
+
+    }
+`
+export const question = styled.div`
+    width: 100%;
+    padding: 20px;
+    border-bottom: 1px solid black;
+    span{
+        font-size: 20px;
+        font-weight: bolder;
+        font-style: italic;
+        margin-right: 15px;
+    }
+`
+
+export const answers = styled.div`
+    padding: 20px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+`
+
+export const questionBody = styled.div`
+    min-height: 400px;
+    border-bottom: 2px solid black;
+    width: 100%;
+`
+
+
